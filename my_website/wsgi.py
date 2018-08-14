bc81c49908c13a668a7b2492fb62f0a154ac378e
Kripanshu <kripanshubhargava@gmail.com>
2018-08-14 15:50:58 +0000

    update settings file in the wsgi

--- my_website/wsgi.py
+++ my_website/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_website.settings")
+import sys
+sys.path.append('../')
+#for p in sys.path: print(p)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_website.prod_settings")
 
 application = get_wsgi_application()
